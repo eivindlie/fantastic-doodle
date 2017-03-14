1f9e6197358b9a73e986f098bd2f62ac989c4735
DSandberg93 <Danisan@stud.ntnu.no>
2017-03-14 13:59:05 +0100

    Added partially finished PrintWorkout class

--- src/dbproject/usecases/PrintWorkoutWeek.java
+++ src/dbproject/usecases/PrintWorkoutWeek.java
@@ -1,5 +1,32 @@
 package dbproject.usecases;
 
-public class PrintWorkoutWeek {
+import java.util.List;
+import java.util.Scanner;
+
+import dbproject.db.Workout;
+
+public class PrintWorkoutWeek implements UseCaseInterface{
+
+	@Override
+	public void runUseCase(Scanner scanner) {
+		System.out.println("Enter the date the week begins, master:");
+		System.out.print(">>>");
+		String weekStart = scanner.nextLine();
+		System.out.println("Enter the date the week ends, master:");
+		System.out.print(">>>");
+		String weekEnd = scanner.nextLine();
+		List<Workout> week = Workout.workoutWeek(weekStart, weekEnd);
+		if(week.isEmpty()) {
+			System.out.println("There are no workouts that week, master!");
+			return;
+		}
+		
+		System.out.println("Here is a list of all the workouts that week, master:");
+		for(Workout workout : week) {
+			System.out.println();
+			System.out.println("Name: \t\t\t");
+			
+		}
+	}
 
 }
