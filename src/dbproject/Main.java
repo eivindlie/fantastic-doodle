58f6ad3f52da68d3089667704a8af2688a5d6f9b
Eivind Lie Andreassen <ela4991@gmail.com>
2017-03-14 12:41:42 +0100

    Implemented print help usecase

--- src/dbproject/Main.java
+++ src/dbproject/Main.java
@@ -3,6 +3,7 @@ package dbproject;
 import dbproject.db.DBConnector;
 import dbproject.usecases.AddExercise;
 import dbproject.usecases.PrintExercise;
+import dbproject.usecases.PrintHelp;
 import dbproject.usecases.UseCaseInterface;
 
 import java.sql.Connection;
@@ -16,11 +17,13 @@ public class Main {
     DBConnector dbConnector;
     UseCaseInterface addExerciseUsecase;
     UseCaseInterface printExercisesUsecase;
+    UseCaseInterface printHelpUsecase;
 
     public Main() {
         dbConnector = new DBConnector();
         addExerciseUsecase = new AddExercise();
         printExercisesUsecase = new PrintExercise();
+        printHelpUsecase = new PrintHelp();
     }
 
     public void run() {
@@ -32,7 +35,7 @@ public class Main {
             while(!input.equals("exit")) {
                 switch(input) {
                     case "help":
-
+                        printHelpUsecase.runUseCase(scanner);
                         break;
                     case "addExercise":
                         addExerciseUsecase.runUseCase(scanner);
---
+++ src/dbproject/usecases/PrintHelp.java
@@ -0,0 +1,25 @@
+package dbproject.usecases;
+
+import java.util.Scanner;
+
+/**
+ * Created by ela49 on 14.03.2017.
+ */
+public class PrintHelp implements UseCaseInterface {
+    @Override
+    public void runUseCase(Scanner scanner) {
+        String[][] cmds = {
+                {"addExercise", "Adds a new exercise"},
+                {"help", "Prints this help menu"},
+                {"printExercises", "Prints all exercises currently in the database"},
+            };
+
+        System.out.println();
+        System.out.println("Here are all the commands available for you, master:");
+
+        for(String[] row : cmds) {
+            System.out.format("%-20s%s\n", row);
+        }
+        System.out.println();
+    }
+}
