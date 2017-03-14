7dbafde0add70abb371e3f3bfbb387b3959dfcfd
Eivind Lie Andreassen <ela4991@gmail.com>
2017-03-14 13:00:57 +0100

    Removed unused testQuery

--- src/dbproject/Main.java
+++ src/dbproject/Main.java
@@ -52,24 +52,6 @@ public class Main {
         }
     }
 
-    private void testQuery() throws SQLException {
-        dbConnector.connect();
-
-        Connection conn = dbConnector.getConnection();
-        Statement statement = conn.createStatement();
-        statement.execute("CREATE TABLE testTable(" +
-                    "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                    "name VARCHAR(50)" +
-                ")");
-        statement.execute("INSERT INTO testTable('name') VALUES ('Eivind')");
-        ResultSet rs = statement.executeQuery("SELECT * FROM testTable");
-        while(rs.next()) {
-            System.out.println(rs.getString("id") + ": " + rs.getString("name"));
-        }
-
-        dbConnector.close();
-    }
-
     public static void main(String... args) {
         Main main = new Main();
         main.run();
